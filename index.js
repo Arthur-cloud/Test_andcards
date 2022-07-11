63bd4630d8b3cd5be48d5746874e08ffc96cb4cb
Arthur <64473520+Arthur-cloud@users.noreply.github.com>
2022-07-11 20:03:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,13 +8,12 @@ async function recursiveSearch(dir) {
 
     const directoryContents = await readdir(dir, { withFileTypes: true });
     let folder = 0
-    let file = 0 // Set the value to one if you want the json file to be counted too
+    let file = 0
 
     writeFile(dir + '\\info.json', JSON.stringify({dir : dir, file : file, folder : folder }),(err) => {console.log(err)})
-    console.log(`A new info.json file has been created on the path: ${dir + '\\info.json'}`)
 
-    directoryContents.map((filePath) => {
-        const res = resolve(dir, filePath.name);
+    let result = await Promise.all(directoryContents.map((filePath) => {
+        let res = resolve(dir, filePath.name);
 
         if(filePath.isDirectory()) folder += 1
         if(filePath.isFile()) file += 1
@@ -26,8 +25,11 @@ async function recursiveSearch(dir) {
         } else {
             return recursiveSearch(res)
         }
-    });
+    }));
+    return Array.prototype.concat(...result)
 } 
 
 recursiveSearch(dir)
+    .then(result => console.log(result))
+    .catch(err => console.log(err))
 
